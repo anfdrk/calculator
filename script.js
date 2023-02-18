f09c48361cc9efb0b67a29cb11efde085e05bf0c
Andrei Fedaruk <anfdrk@gmail.com>
2023-02-19 00:42:41 +0300

    Create the initial setup of the calculator
    Created basic functions (addition, subtraction, multiplication,
    division) and html with calculator buttons

---
+++ script.js
@@ -0,0 +1,31 @@
+const defaultDisplayValue = '0'
+
+
+function add(x, y) {
+  return x + y
+}
+
+function subtract(x, y) {
+  return x - y
+}
+
+function multiply(x, y) {
+  return x * y
+}
+
+function divide(x, y) {
+  return x / y
+}
+
+function operate (operator, x, y) {
+  if (operator === '+') {
+    return add(x, y)
+  } else if (operator === '-') {
+    return subtract(x, y)
+  } else if (operator === '*') {
+    return multiply(x, y)
+  } else if (operator === '/') {
+    return divide(x, y)
+  }
+}
+
